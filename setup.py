ace1ee4adab9d6f06f1fef744dc6a20e53e09222
Tom Morris <tommorris512@icloud.com>
2023-12-29 18:58:40 +0000

    Added a setup file to install all necessary dependencies.

---
+++ setup.py
@@ -0,0 +1,9 @@
+import subprocess
+import nltk
+
+#install bs4 and requests
+subprocess.run(['pip', 'install', 'requests'])
+subprocess.run(['pip', 'install', 'bs4'])
+
+# Download NLTK resources
+nltk.download('averaged_perceptron_tagger')
